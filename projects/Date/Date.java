f9254226e7fafa7a339c9ccad5b5e2cd7f15920b
malikfloyd1 <malikfloyd330@yahoo.com>
2025-03-11 15:10:44 -0700

    Update Date.java

--- projects/Date/Date.java
+++ projects/Date/Date.java
@@ -1,20 +1,46 @@
 /**
  * Description here.
- * @author First Last
+ * @author Malik Floyd
  */
 public class Date {
   
   // Add JavaDoc
   public static boolean isValid(int year, int month, int day) {
     boolean isValid = false;
-    // Complete code
+    if(1 <= month && month <= 12){
+      switch (month){
+        case 1, 3, 5, 7, 8, 10, 12:
+          if(1 <= day && day <= 31){
+          isValid = true;
+        }
+          break;
+        case 4, 6, 9, 11:
+          if( 1 <= day && day <= 30){
+          isValid = true;
+        }
+          break;
+        case 2:
+          if(isLeapYear(year)){
+            if ( 1 <= day && day <= 29){
+              isValid = true;
+            }
+              break;
+            else{
+              if ( 1 <= day && day <= 28){
+                inValid = true;
+              }
+            break;
+          }
+    }
     return isValid;
   }
   
   // Add JavaDoc
   public static String toString(int year, int month, int day) {
     String date = "";
-    // Complete code. Verify date is valid.
+    if(isValid(year, month, day));{
+      return String.format("%04d-%02d-%02d);
+    }
     return date;
   }
   
